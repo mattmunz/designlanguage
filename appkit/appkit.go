3f1cee954e3012b0367fc2ba234d2ac38f60c3ad
Matt Munz <matt.m.munz@gmail.com>
2026-06-07 17:05:04 -0700

    Code cleanup

--- appkit/appkit.go
+++ appkit/appkit.go
@@ -2,6 +2,7 @@
 package appkit
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
@@ -14,58 +15,69 @@ import (
 	klog "github.com/go-kit/kit/log"
 )
 
-// TODO All the package-level state is gross. Make an object or something.
 var (
-	app     model.App
-	cli     model.CLI
 	cfgFile string
-	logger  klog.Logger
 )
 
-func Logger() klog.Logger {
-	return logger
-}
-
-func DoInit(app1 model.App, cli1 model.CLI) {
-	app = app1
-	cli = cli1
-	cobra.OnInitialize(initConfig)
-
-	logger = klog.NewLogfmtLogger(klog.NewSyncWriter(os.Stdout))
+func DoInit(cli1 model.CLI) {
+	logger := klog.NewLogfmtLogger(klog.NewSyncWriter(os.Stdout))
 
 	misc.LogMessage(logger, "Logger initialized.")
-}
 
-func initConfig() {
-	if cfgFile != "" {
-		viper.SetConfigFile(cfgFile)
+	cli1.SetLogger(logger)
+
+	misc.LogMessage(cli1.Logger(), "CLI initialized.")
+
+	initConfig2 := func() {
+		if cfgFile != "" {
+			viper.SetConfigFile(cfgFile)
+		}
+
+		viper.SetConfigName(cli1.App().ConfigName())
+		viper.AddConfigPath("$HOME")
+		viper.AutomaticEnv()
+
+		if err := viper.ReadInConfig(); err == nil {
+			misc.LogMessage(cli1.Logger(), fmt.Sprintf("Using config file: %s", viper.ConfigFileUsed()))
+		}
 	}
 
-	viper.SetConfigName(app.ConfigName())
-	viper.AddConfigPath("$HOME")
-	viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err == nil {
-		// TODO Log
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
+	cobra.OnInitialize(initConfig2)
 }
 
-func NewCommandBase(logger klog.Logger) *cobra.Command {
-	newCmd := &cobra.Command{
-		Use:   cli.Name(),
-		Short: cli.ShortDescription(),
-	}
-	newCmd.AddCommand(newVersionCommand(logger))
-	newCmd.PersistentFlags().StringVar(&cfgFile, "config", "",
-		fmt.Sprintf("config file (default is $HOME/%s.yaml)", app.ConfigName()))
-
-	return newCmd
+type cliImpl struct {
+	app              model.App
+	name             string
+	shortDescription string
+	logger           klog.Logger
 }
 
-// TODO Maybe make method on CLI.
-func Execute(cli model.CLI) {
-	rootCmd, err := cli.NewRootCommand(logger)
+func (c *cliImpl) App() model.App {
+	return c.app
+}
+
+func (c *cliImpl) Name() string {
+	return c.name
+}
+
+func (c *cliImpl) ShortDescription() string {
+	return c.shortDescription
+}
+
+func (c *cliImpl) Logger() klog.Logger {
+	return c.logger
+}
+
+func (c *cliImpl) SetLogger(logger klog.Logger) {
+	c.logger = logger
+}
+
+func (c *cliImpl) NewRootCommand() (*cobra.Command, error) {
+	return nil, errors.New("Not implemented")
+}
+
+func (c *cliImpl) Execute() {
+	rootCmd, err := c.NewRootCommand()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
@@ -77,15 +89,48 @@ func Execute(cli model.CLI) {
 	}
 }
 
-func newVersionCommand(logger klog.Logger) *cobra.Command {
-	return &cobra.Command{
-		Use:   "version",
-		Short: "Show the version number of this command.",
-		Run:   cmd.WrapRunner(doVersion, logger),
+func NewCLI(app model.App, name, shortDescription string) model.CLI {
+	return &cliImpl{
+		app:              app,
+		name:             name,
+		shortDescription: shortDescription,
+	}
+}
+func NewCommandBase(cli model.CLI) *cobra.Command {
+	newCmd := &cobra.Command{
+		Use:   cli.Name(),
+		Short: cli.ShortDescription(),
+	}
+	newCmd.AddCommand(newVersionCommand(cli))
+	newCmd.PersistentFlags().StringVar(&cfgFile, "config", "",
+		fmt.Sprintf("config file (default is $HOME/%s.yaml)", cli.App().ConfigName()))
+
+	return newCmd
+}
+
+// TODO Maybe make method on CLI.
+func Execute(cli model.CLI) {
+	rootCmd, err := cli.NewRootCommand()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
 	}
 }
 
-func doVersion(_ klog.Logger, _ *cobra.Command, _ []string) error {
-	fmt.Println(app.Version())
+func newVersionCommand(cli model.CLI) *cobra.Command {
+	return &cobra.Command{
+		Use:   "version",
+		Short: "Show the version number of this command.",
+		Run:   cmd.WrapRunner(doVersion, cli),
+	}
+}
+
+func doVersion(cli model.CLI, _ *cobra.Command, _ []string) error {
+	fmt.Println(cli.App().Version())
 	return nil
 }
--- appkit/cmd/cmd.go
+++ appkit/cmd/cmd.go
@@ -4,15 +4,15 @@ import (
 	"fmt"
 	"os"
 
-	klog "github.com/go-kit/kit/log"
+	"github.com/mattmunz/designlanguage/appkit/model"
 	"github.com/spf13/cobra"
 )
 
 type commandRunner func(cmd *cobra.Command, args []string)
 
-func WrapRunner(run func(klog.Logger, *cobra.Command, []string) error, logger klog.Logger) commandRunner {
+func WrapRunner(run func(_ model.CLI, _ *cobra.Command, _ []string) error, cli model.CLI) commandRunner {
 	return func(command *cobra.Command, args []string) {
-		if err := run(logger, command, args); err != nil {
+		if err := run(cli, command, args); err != nil {
 			fmt.Fprintf(os.Stderr, "ERROR: %+v\n", err)
 			os.Exit(1)
 		}
